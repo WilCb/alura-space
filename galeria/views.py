e431d7ffefdedd43ed71b24775cac0a5d81dc21b
Williams <williams.lima989@gmail.com>
2025-05-13 22:06:07 -0300

    criação da pasta partials e separação dos templates

--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    title = 'Alura Space - Galeria'
+    return render(request, 'galeria/index.html', {'title': title})
+
+def imagem(request):
+    title = 'Alura Space - Imagem'
+    return render(request, 'galeria/imagem.html', {'title': title})
